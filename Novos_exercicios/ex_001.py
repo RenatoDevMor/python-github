ed444ff64bb0e6f6c71140e97adc3cedd361933c
Renato Moreira <131251056+RenatoMor@users.noreply.github.com>
2024-03-28 09:05:09 +0000

    Enviando novas sugestões

---
+++ Novos_exercicios/ex_001.py
@@ -0,0 +1,21 @@
+# Exemplo de algoritmo com variáveis e listas
+
+# Declaração de variáveis
+nome = "João"
+idade = 25
+altura = 1.75
+
+# Declaração de uma lista
+numeros = [1, 2, 3, 4, 5]
+
+# Acessando elementos da lista
+primeiro_numero = numeros[0]
+ultimo_numero = numeros[-1]
+
+# Imprimindo os valores das variáveis e da lista
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Números:", numeros)
+print("Primeiro número:", primeiro_numero)
+print("Último número:", ultimo_numero)
